cfa93f6def19c84f48030ba3db135d12c0b4b6e8
yazhini2706 <yaminimunusamy2706@gmail.com>
2025-02-22 16:53:27 +0530

    Add files via upload
    30+ Lines of codes

---
+++ Basic-1.py
@@ -0,0 +1,61 @@
+# Output
+name="python"
+print(name)
+print(type(name))
+
+# Output
+year=1991
+print(year)
+print(type(year))
+
+# capitalize() -> It capitalizes the first character of the string
+print("It capitalizes the first character of the string")
+basic=name.capitalize()
+print(basic)
+print(type(basic))
+
+# Input and Output
+first_name=input("Enter your firstname:")
+print(type(first_name))
+middle_name=input("Enter your middle name:")
+print(type(middle_name))
+last_name=input("Enter your lastname:")
+print(type(last_name))
+
+# Using f-string
+print(f"My first name is {first_name} and my middle name is {middle_name} and my last name is {last_name}")
+
+# isalpha() ->It returns true if all the characters are alphabets and there is at least one character, otherwise False.
+print("It returns true if all the characters are alphabets and there is at least one character, otherwise False.")
+basic=name.isalpha()
+print(basic)
+print(type(basic))
+
+# islower() -> It returns true if the characters of a string are in lower case, otherwise false.
+print("It returns true if the characters of a string are in lower case, otherwise false.")
+basic=name.islower()
+print(basic)
+print(type(basic))
+
+# isnumeric() -> It returns true if the string contains only numeric characters.
+print("It returns true if the string contains only numeric characters.")
+basic=name.isnumeric()
+print(basic)
+print(type(basic))
+
+# isprintable() -> It returns true if all the characters of s are printable or s is empty, false otherwise.
+print("It returns true if all the characters of s are printable or s is empty, false otherwise.")
+basic=name.isprintable()
+print(type(basic))
+print(basic)
+
+# isupper() -> It returns false if characters of a string are in Upper case, otherwise False.
+print("It returns false if characters of a string are in Upper case, otherwise False.")
+basic=name.isupper()
+print(basic)
+print(type(basic))
+
+
+
+
+
